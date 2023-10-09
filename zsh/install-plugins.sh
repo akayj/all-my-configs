7e75eb089f98468154eb0feee2dafd1d34cf7ea7
akayj <askingyj@gmail.com>
2023-10-09 14:57:44 +0800

    use bash

---
+++ zsh/install-plugins.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+declare -A plugins
+plugins=(
+  [zsh-autocomplete]=marlonrichert/zsh-autocomplete
+  [you-should-use]=MichaelAquilina/zsh-you-should-use
+  [F-Sy-H]=z-shell/F-Sy-H
+  [zsh-autosuggestions]=zsh-users/zsh-autosuggestions
+  [zsh-syntax-highlighting]=zsh-users/zsh-syntax-highlighting
+)
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+for name in ${!plugins[@]}; do
+  if [ ! -d "$ZSH_CUSTOM/plugins/$name" ]; then
+    git clone --depth=1 "https://github.com/${plugins[$name]}.git" "$ZSH_CUSTOM/plugins/$name"
+  else
+    echo "$name already installed"
+  fi
+done
