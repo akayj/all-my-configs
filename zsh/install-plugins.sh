3a51c5908ad5d5b43d23db85b8af21ed88bf927c
Jian Yu <askingyj@gmail.com>
2025-02-28 17:11:59 +0800

    Update install-plugins.sh

--- zsh/install-plugins.sh
+++ zsh/install-plugins.sh
@@ -2,19 +2,21 @@
 
 set -eu
 
-declare -A plugins
+# 使用普通数组来存储键值对
 plugins=(
-  [you-should-use]=MichaelAquilina/zsh-you-should-use
-  [F-Sy-H]=z-shell/F-Sy-H
-  [zsh-autosuggestions]=zsh-users/zsh-autosuggestions
-  [zsh-syntax-highlighting]=zsh-users/zsh-syntax-highlighting
+  "you-should-use MichaelAquilina/zsh-you-should-use"
+  "F-Sy-H z-shell/F-Sy-H"
+  "zsh-autosuggestions zsh-users/zsh-autosuggestions"
+  "zsh-syntax-highlighting zsh-users/zsh-syntax-highlighting"
 )
 
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
-for name in ${!plugins[@]}; do
+for plugin in "${plugins[@]}"; do
+  # 分割键值对
+  IFS=' ' read -r name repo <<< "$plugin"
   if [ ! -d "$ZSH_CUSTOM/plugins/$name" ]; then
-    git clone --depth=1 "https://github.com/${plugins[$name]}.git" "$ZSH_CUSTOM/plugins/$name"
+    git clone --depth=1 "https://github.com/$repo.git" "$ZSH_CUSTOM/plugins/$name"
   else
     echo "$name already installed"
   fi
